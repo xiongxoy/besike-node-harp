b901400b9c8067f2dcc09f695e3362f7b58d7180
xiongxoy <xiong.xoy@gmail.com>
2015-01-06 20:52:09 +0800

    Reject Stupid Requests

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var connect = require('connect');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
 var makeRewriteURL = require('./lib/processor/rewriteURL');
+var makeRejectStupidRequest = require('./lib/processor/rejectStupidRequest');
 
 function createMiniHarp(root) {
   var app = connect();
+  app.use(makeRejectStupidRequest());
   app.use(makeRewriteURL());
   app.use(makeJade(root));
   app.use(makeLess(root));
