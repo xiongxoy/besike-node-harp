ea42813b84243b9e77246d088c7afb40f8288b1a
xiongxoy <xiong.xoy@gmail.com>
2015-01-06 20:37:26 +0800

    Requesting for the root should render /index.html

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ module.exports = createMiniHarp;
 var connect = require('connect');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
+var makeRewriteURL = require('./lib/processor/rewriteURL');
 
 function createMiniHarp(root) {
   var app = connect();
+  app.use(makeRewriteURL());
   app.use(makeJade(root));
   app.use(makeLess(root));
+
   return app;
 }
 
